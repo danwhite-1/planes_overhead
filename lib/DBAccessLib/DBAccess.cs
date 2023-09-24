6f1af8f0bcb3c8926d39a786bb3df68a2ad7e01e
Daniel White <daniel.j.white999@gmail.com>
2023-09-24 15:42:34 +0100

    Reorder, and reorganise functions in dbaccess

--- lib/DBAccessLib/DBAccess.cs
+++ lib/DBAccessLib/DBAccess.cs
@@ -6,6 +6,7 @@ namespace DBAccessLib;
 
 public class DBAccess
 {
+    //! Constructor
     public DBAccess()
     {
         var builder = new MySqlConnectionStringBuilder
@@ -22,43 +23,9 @@ public class DBAccess
         VerifyTablesExist();
     }
 
-    public void WriteFlightsToDB(List<Flight> flights, long timestamp)
-    {
-        if (flights.Count == 0) { return; }
-
-        string insert_sql = $"INSERT INTO flights(query_timestamp, transponderid, callsign, origin_country, timestamp, last_contact, latitude, longitude, baro_altitude, on_ground, velocity, true_track, vertical_rate, sensors, geo_altitude, squak, spi, position_source, category) VALUES";
-        var cmd = new MySqlCommand(insert_sql, conn);
-
-        int count = 0;
-        foreach (var flight in flights) 
-        {
-            string values_sql = $"({timestamp}, @transponderid{count}, @callsign{count}, @origin_country{count}, @timestamp{count}, @last_contact{count}, @latitude{count}, @longitude{count}, @baro_altitude{count}, @on_ground{count}, @velocity{count}, @true_track{count}, @vertical_rate{count}, @sensors{count}, @geo_altitude{count}, @squak{count}, @spi{count}, @position_source{count}, @category{count}),";
-            cmd.CommandText += values_sql;
-            cmd.Parameters.AddWithValue($"@transponderid{count}", flight.TransponderId);
-            cmd.Parameters.AddWithValue($"@callsign{count}", flight.Callsign);
-            cmd.Parameters.AddWithValue($"@origin_country{count}", flight.Origin_country);
-            cmd.Parameters.AddWithValue($"@timestamp{count}", flight.Timestamp);
-            cmd.Parameters.AddWithValue($"@last_contact{count}", flight.Last_contact);
-            cmd.Parameters.AddWithValue($"@latitude{count}", flight.Latitude);
-            cmd.Parameters.AddWithValue($"@longitude{count}", flight.Longitude);
-            cmd.Parameters.AddWithValue($"@baro_altitude{count}", flight.Baro_altitude);
-            cmd.Parameters.AddWithValue($"@on_ground{count}", flight.On_ground);
-            cmd.Parameters.AddWithValue($"@velocity{count}", flight.Velocity);
-            cmd.Parameters.AddWithValue($"@true_track{count}", flight.True_track);
-            cmd.Parameters.AddWithValue($"@vertical_rate{count}", flight.Vertical_rate);
-            cmd.Parameters.AddWithValue($"@sensors{count}", flight.Sensors);
-            cmd.Parameters.AddWithValue($"@geo_altitude{count}", flight.Geo_altitude);
-            cmd.Parameters.AddWithValue($"@squak{count}", flight.Squak);
-            cmd.Parameters.AddWithValue($"@spi{count}", flight.Spi);
-            cmd.Parameters.AddWithValue($"@position_source{count}", flight.Position_source);
-            cmd.Parameters.AddWithValue($"@category{count}", flight.Category);
-            count++;
-        }
-        cmd.CommandText = cmd.CommandText.TrimEnd(',');
-        var rows = cmd.ExecuteNonQuery();
-        Console.WriteLine($"{rows} written to db. Number of flights: {flights.Count}");
-    }
-
+    ///////////////////////////
+    //! Create Table Functions
+    ///////////////////////////
     public void CreateFlightTable()
     {
         string createTable_sql = @$"CREATE TABLE IF NOT EXISTS `flights`
@@ -99,6 +66,116 @@ PRIMARY KEY (flightid)
         }
     }
 
+    public void CreateZoneMatchTable()
+    {
+        string createTable_sql = @$"CREATE TABLE IF NOT EXISTS `zonematches`
+(
+`zonematchid` INT NOT NULL AUTO_INCREMENT,
+`flightid` INT NOT NULL,
+`zoneid` INT NOT NULL,
+PRIMARY KEY (zonematchid)
+);";
+
+        var cmd = new MySqlCommand(createTable_sql, conn);
+        cmd.ExecuteNonQuery();
+
+        string checkExists_sql = $"SELECT COUNT(*) FROM information_schema.tables WHERE table_name='zonematches'";
+        cmd = new MySqlCommand(checkExists_sql, conn);
+        var rows = cmd.ExecuteScalar();
+
+        if (Convert.ToInt16(rows) != 1)
+        {
+            throw new Exception("zonematches table not able to be created");
+        }
+    }
+
+    public void CreateSearchZonesTable()
+    {
+        string createTable_sql = @$"CREATE TABLE IF NOT EXISTS `searchzones`
+(
+`searchzoneid` INT NOT NULL AUTO_INCREMENT,
+`point` VARCHAR(40) NOT NULL,
+`distance` INT NOT NULL,
+PRIMARY KEY (searchzoneid)
+);";
+
+        var cmd = new MySqlCommand(createTable_sql, conn);
+        cmd.ExecuteNonQuery();
+
+        string checkExists_sql = $"SELECT COUNT(*) FROM information_schema.tables WHERE table_name='searchzones'";
+        cmd = new MySqlCommand(checkExists_sql, conn);
+        var rows = cmd.ExecuteScalar();
+
+        if (Convert.ToInt16(rows) != 1)
+        {
+            throw new Exception("searchzones table not able to be created");
+        }
+    }
+
+    ////////////////////
+    //! Write Functions
+    ////////////////////
+    public void WriteFlightsToDB(List<Flight> flights, long timestamp)
+    {
+        if (flights.Count == 0) { return; }
+
+        string insert_sql = $"INSERT INTO flights(query_timestamp, transponderid, callsign, origin_country, timestamp, last_contact, latitude, longitude, baro_altitude, on_ground, velocity, true_track, vertical_rate, sensors, geo_altitude, squak, spi, position_source, category) VALUES";
+        var cmd = new MySqlCommand(insert_sql, conn);
+
+        int count = 0;
+        foreach (var flight in flights) 
+        {
+            string values_sql = $"({timestamp}, @transponderid{count}, @callsign{count}, @origin_country{count}, @timestamp{count}, @last_contact{count}, @latitude{count}, @longitude{count}, @baro_altitude{count}, @on_ground{count}, @velocity{count}, @true_track{count}, @vertical_rate{count}, @sensors{count}, @geo_altitude{count}, @squak{count}, @spi{count}, @position_source{count}, @category{count}),";
+            cmd.CommandText += values_sql;
+            cmd.Parameters.AddWithValue($"@transponderid{count}", flight.TransponderId);
+            cmd.Parameters.AddWithValue($"@callsign{count}", flight.Callsign);
+            cmd.Parameters.AddWithValue($"@origin_country{count}", flight.Origin_country);
+            cmd.Parameters.AddWithValue($"@timestamp{count}", flight.Timestamp);
+            cmd.Parameters.AddWithValue($"@last_contact{count}", flight.Last_contact);
+            cmd.Parameters.AddWithValue($"@latitude{count}", flight.Latitude);
+            cmd.Parameters.AddWithValue($"@longitude{count}", flight.Longitude);
+            cmd.Parameters.AddWithValue($"@baro_altitude{count}", flight.Baro_altitude);
+            cmd.Parameters.AddWithValue($"@on_ground{count}", flight.On_ground);
+            cmd.Parameters.AddWithValue($"@velocity{count}", flight.Velocity);
+            cmd.Parameters.AddWithValue($"@true_track{count}", flight.True_track);
+            cmd.Parameters.AddWithValue($"@vertical_rate{count}", flight.Vertical_rate);
+            cmd.Parameters.AddWithValue($"@sensors{count}", flight.Sensors);
+            cmd.Parameters.AddWithValue($"@geo_altitude{count}", flight.Geo_altitude);
+            cmd.Parameters.AddWithValue($"@squak{count}", flight.Squak);
+            cmd.Parameters.AddWithValue($"@spi{count}", flight.Spi);
+            cmd.Parameters.AddWithValue($"@position_source{count}", flight.Position_source);
+            cmd.Parameters.AddWithValue($"@category{count}", flight.Category);
+            count++;
+        }
+        cmd.CommandText = cmd.CommandText.TrimEnd(',');
+        var rows = cmd.ExecuteNonQuery();
+        Console.WriteLine($"{rows} written to db. Number of flights: {flights.Count}");
+    }
+
+    public void WriteZoneMatchesToDB(List<ZoneMatch> zonematches)
+    {
+        if (zonematches.Count == 0) { return; }
+
+        string insert_sql = $"INSERT INTO zonematches(flightid, zoneid) VALUES";
+        var cmd = new MySqlCommand(insert_sql, conn);
+
+        int count = 0;
+        foreach (var zonematch in zonematches) 
+        {
+            string values_sql = $"(@flightid{count}, @zoneid{count}),";
+            cmd.CommandText += values_sql;
+            cmd.Parameters.AddWithValue($"@flightid{count}", zonematch.flight.Id);
+            cmd.Parameters.AddWithValue($"@zoneid{count}", zonematch.searchzone.Id);
+            count++;
+        }
+        cmd.CommandText = cmd.CommandText.TrimEnd(',');
+        var rows = cmd.ExecuteNonQuery();
+        Console.WriteLine($"{rows} written to db. Number of zone matches: {zonematches.Count}");
+    }
+
+    /////////////////////
+    //! Select Functions
+    /////////////////////
     public List<Flight> GetAllFlightsForTimestamp(long ts)
     {
         MySqlCommand cmd = conn.CreateCommand();
@@ -162,73 +239,9 @@ PRIMARY KEY (flightid)
         return sz;
     }
 
-    public void WriteZoneMatchesToDB(List<ZoneMatch> zonematches)
-    {
-        if (zonematches.Count == 0) { return; }
-
-        string insert_sql = $"INSERT INTO zonematches(flightid, zoneid) VALUES";
-        var cmd = new MySqlCommand(insert_sql, conn);
-
-        int count = 0;
-        foreach (var zonematch in zonematches) 
-        {
-            string values_sql = $"(@flightid{count}, @zoneid{count}),";
-            cmd.CommandText += values_sql;
-            cmd.Parameters.AddWithValue($"@flightid{count}", zonematch.flight.Id);
-            cmd.Parameters.AddWithValue($"@zoneid{count}", zonematch.searchzone.Id);
-            count++;
-        }
-        cmd.CommandText = cmd.CommandText.TrimEnd(',');
-        var rows = cmd.ExecuteNonQuery();
-        Console.WriteLine($"{rows} written to db. Number of zone matches: {zonematches.Count}");
-    }
-
-    public void CreateZoneMatchTable()
-    {
-        string createTable_sql = @$"CREATE TABLE IF NOT EXISTS `zonematches`
-(
-`zonematchid` INT NOT NULL AUTO_INCREMENT,
-`flightid` INT NOT NULL,
-`zoneid` INT NOT NULL,
-PRIMARY KEY (zonematchid)
-);";
-
-        var cmd = new MySqlCommand(createTable_sql, conn);
-        cmd.ExecuteNonQuery();
-
-        string checkExists_sql = $"SELECT COUNT(*) FROM information_schema.tables WHERE table_name='zonematches'";
-        cmd = new MySqlCommand(checkExists_sql, conn);
-        var rows = cmd.ExecuteScalar();
-
-        if (Convert.ToInt16(rows) != 1)
-        {
-            throw new Exception("zonematches table not able to be created");
-        }
-    }
-
-    public void CreateSearchZonesTable()
-    {
-        string createTable_sql = @$"CREATE TABLE IF NOT EXISTS `searchzones`
-(
-`searchzoneid` INT NOT NULL AUTO_INCREMENT,
-`point` VARCHAR(40) NOT NULL,
-`distance` INT NOT NULL,
-PRIMARY KEY (searchzoneid)
-);";
-
-        var cmd = new MySqlCommand(createTable_sql, conn);
-        cmd.ExecuteNonQuery();
-
-        string checkExists_sql = $"SELECT COUNT(*) FROM information_schema.tables WHERE table_name='searchzones'";
-        cmd = new MySqlCommand(checkExists_sql, conn);
-        var rows = cmd.ExecuteScalar();
-
-        if (Convert.ToInt16(rows) != 1)
-        {
-            throw new Exception("searchzones table not able to be created");
-        }
-    }
-
+    //////////////////////
+    //! Utility Functions
+    //////////////////////
     public void VerifyTablesExist()
     {
         var tableMap = new Dictionary<string, Action>()
